ec42c0df8631f2bf2767623e65d21e95a12a097b
Chi Hien <chihien2002@gmail.com>
2024-03-22 20:57:44 +0700

    dev_'add healthcheck api for deploy checking'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { HealthcheckModule } from './healthcheck/healthcheck.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [AuthModule, PrismaModule, HealthcheckModule],
 })
 export class AppModule {}
